8cd9e1495128a86e672cd081fcd377addfdd72e8
Andreas <andreas.gemsa@googlemail.com>
2019-02-28 22:30:01 +0100

    script with exiting code

--- convert.sh
+++ convert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xe
 cargo build --verbose
 wget https://download.geofabrik.de/europe/germany/bremen-latest.osm.pbf
 target/release/osmtoroadgraph --verbose -n c bremen-latest.osm.pbf
