1a8f6b351c78f918ab8d94b0e9fb724243951a98
Andreas <andreas.gemsa@googlemail.com>
2019-02-28 22:44:17 +0100

    build release

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
-cargo build --verbose
+cargo build --release
 wget https://download.geofabrik.de/europe/germany/bremen-latest.osm.pbf
 target/release/osmtoroadgraph --verbose -n c bremen-latest.osm.pbf
